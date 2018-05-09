f6cddabcbfd19db508f5fdd76ab8b57ed0e223cb
Vanderson Fábio <vanderson.fabio@gmail.com>
2018-05-09 15:54:59 -0300

    Carga inicial do SiGP

---
+++ src/test/java/com/pmrn/sigp/SigpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pmrn.sigp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SigpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
